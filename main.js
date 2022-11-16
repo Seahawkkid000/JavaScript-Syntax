fc2ced85e2622c067e5d614493b71d61ca739063
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2022-11-15 18:00:04 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var First_Name = "Luka"
+
+console.log(First_Name)
+
+var Second_Name = "Mahomes"
+
+console.log(Second_Name)
+
+var Full_Name = First_Name + " " + Second_Name
+
+console.log(Full_Name)
+
+var Len = Full_Name.length
+
+console.log(Len)
+
+var Rep = Full_Name.replace("Mahomes", "Herbert")
+
+console.log(Rep)
+
+var Upper = Rep.toUpperCase()
+
+console.log(Upper)
+
+var Lower = Upper.toLowerCase()
+
+console.log(Lower)
+
+var Number_One = Math.pow(6,5)
+
+console.log(Number_One)
+
+var Maximium = Math.max(26,57)
+
+console.log(Maximium)
+
+var Minimum = Math.min(64,96)
+
+console.log(Minimum)
+
+var Round = Math.round(-456.6584)
+
+console.log(Round)
